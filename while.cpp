dbcbfbae317e6b11e3583804bc89525f7a2427d3
pabloherrera17 <pablonestorherrera26@gmail.com>
2024-04-18 15:52:16 -0300

    clase de ciclos

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int valoringresado= 10;
+
+
+for (int i=0; i<= valoringresado; i++){
+  if(i%2==0){
+    cout<< i <<endl;
+  }
+
+}
+return 0;
+}
+
